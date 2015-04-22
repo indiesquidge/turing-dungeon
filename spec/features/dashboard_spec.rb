5210b229f257a788d2eed6ba97a907862d5cda78
Austin Wood <i@austinwood.me>
2015-04-22 11:48:40 -0600

    Refactor specs to match new style design

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -3,15 +3,15 @@ require "rails_helper"
 RSpec.describe "Dashboard", type: :feature do
   it "displays a Twitter feed" do
     visit root_path
-    within("ul#twitter-feed") do
-      expect(page).to have_content("here is a tweet")
+    within("#twitter-feed") do
+      expect(page).to have_content("The path of the righteous man")
     end
   end
 
   it "displays last five commits across all users" do
     visit root_path
-    within("ul#github-feed") do
-      expect(page).to have_content("here is a commit")
+    within("#github-feed") do
+      expect(page).to have_content("This is the commit message")
     end
   end
 end
