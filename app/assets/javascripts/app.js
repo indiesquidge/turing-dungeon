5e56ba33e1608cc8c5e10ba5fa37282516ae8db5
Austin Wood <i@austinwood.me>
2015-04-23 16:29:51 -0600

    Mock cohort list with name search filter

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,25 +1,29 @@
 var app = angular.module("turing-dungeon", [  ]);
 
 app.controller("UserController", function($scope) {
-  $scope.todoList = [];
-  $scope.currentTodo = "";
-
-  $scope.add = function() {
-    var todoObject = {
-      name:$scope.currentTodo,
-      complete:false
-    };
-    if ($scope.currentTodo !== "") {
-      $scope.todoList.push(todoObject);
-    }
-    $scope.currentTodo = "";
-  };
-
-  $scope.delete = function(index) {
-    $scope.todoList.splice(index, 1);
-  };
-
-  $scope.finished = function(index) {
-    $scope.todoList[index].complete = !$scope.todoList[index].complete;
-  };
+  $scope.searchName = "";
+  $scope.cohorts =[
+  {
+    "name":"1409",
+    "students": [
+      "danny",
+      "chandra",
+      "alex",
+      "trevor"
+    ],
+    "color":"red",
+    "graduated":true
+  },
+  {
+    "name":"1412",
+    "students": [
+      "trey",
+      "austin",
+      "dj",
+      "kyra"
+    ],
+    "color":"purple",
+    "graduated":false
+  }
+  ];
 });
