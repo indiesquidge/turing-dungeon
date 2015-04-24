3670d330a3c30d7c32fc3caf790250986b3fb097
Austin Wood <i@austinwood.me>
2015-04-23 18:10:51 -0600

    Update specs to use dynamic test data

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -2,16 +2,23 @@ require "rails_helper"
 
 RSpec.describe "Dashboard", type: :feature do
   it "displays a Twitter feed" do
+    tweet = Tweet.create!(message: "This is an exciting Turing tweet!",
+                          owner:   "OHDungeon")
     visit root_path
     within("#twitter-feed") do
-      expect(page).to have_content("The path of the righteous man")
+      expect(page).to have_content(tweet.message)
+      expect(page).to have_content(tweet.owner)
     end
   end
 
-  it "displays last five commits across all users" do
+  it "displays Commits across all users" do
+    user   = User.create!(nickname: "ClassicRichard")
+    commit = Commit.create!(message: "This is a commit",
+                            user_id: user.id)
     visit root_path
     within("#github-feed") do
-      expect(page).to have_content("This is the commit message")
+      expect(page).to have_content(commit.message)
+      expect(page).to have_content(commit.user.nickname)
     end
   end
 end
