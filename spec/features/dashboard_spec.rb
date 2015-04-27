612c52f31f440bba89107a253b03a68693ed1da1
Austin Wood <i@austinwood.me>
2015-04-27 17:08:01 -0600

    Remove tweets table since we are now just pulling all data directly from Twitter

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,16 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "Dashboard", type: :feature do
-  it "displays a Twitter feed" do
-    tweet = Tweet.create!(message: "This is an exciting Turing tweet!",
-                          owner:   "OHDungeon")
-    visit root_path
-    within("#twitter-feed") do
-      expect(page).to have_content(tweet.message)
-      expect(page).to have_content(tweet.owner)
-    end
-  end
-
   it "displays Commits across all users" do
     user   = User.create!(nickname: "ClassicRichard")
     commit = Commit.create!(message: "This is a commit",
