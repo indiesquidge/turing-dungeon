ae31c183c515ee8e2aeaf98339187382cca55fe3
Austin Wood <i@austinwood.me>
2015-04-30 14:44:55 -0600

    Fix controller misspell

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,6 @@
 class ErrorsController < ApplicationController
   def show
     @exception = env["action_dispatch.exception"]
-    rnder request.path[1..-1]
+    render request.path[1..-1]
   end
 end
