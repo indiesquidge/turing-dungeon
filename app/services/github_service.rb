42835eeb7ced03e8cd42f02ad715acf28e344fe8
Austin Wood <i@austinwood.me>
2015-05-02 14:13:12 -0600

    Super shitty API method call, but hey, it works for now, and the view looks cleaner

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -5,10 +5,19 @@ class GithubService
     @connection = Faraday.new(url: "https://api.github.com")
   end
 
-  def push_events
-    User.all.map do |user|
-      parse(connection.get("users/#{user.nickname}/events/public?per_page=1&client_id=#{ENV['GITHUB_ID']}&client_secret=#{ENV['GITHUB_SECRET']}"))
-    end.flatten.select { |event| event[:type] == "PushEvent" }
+  def event_data
+    User.all.each do |user|
+      user_events = parse(connection.get("users/#{user.nickname}/events/public?per_page=5&client_id=#{ENV['GITHUB_ID']}&client_secret=#{ENV['GITHUB_SECRET']}"))
+      user_events.select! { |event| event[:type] == "PushEvent" }
+      user_events.each do |event|
+        event[:payload][:commits].select { |commit| commit[:author][:name] == user.name }.each do |commit|
+          user.commits.create(repo: event[:repo][:name],
+                              message: commit[:message],
+                              sha: commit[:sha],
+                              created_at: event[:created_at])
+        end
+      end
+    end
   end
 
   private
