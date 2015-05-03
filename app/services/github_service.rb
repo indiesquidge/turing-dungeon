bae3592015ff6185680003547a42382d86e745d4
Austin Wood <i@austinwood.me>
2015-05-03 14:31:08 -0600

    Github service now knows nothing about our app, and all database logic is kept from it's methods

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,28 +1,16 @@
 class GithubService
-  attr_reader :connection
-
-  def initialize
-    @connection = Faraday.new(url: "https://api.github.com")
-  end
-
-  def event_data
-    User.all.each do |user|
-      user_events = parse(connection.get("users/#{user.nickname}/events/public?per_page=5&client_id=#{ENV['GITHUB_ID']}&client_secret=#{ENV['GITHUB_SECRET']}"))
-      user_events.select! { |event| event[:type] == "PushEvent" }
-      user_events.each do |event|
-        event[:payload][:commits].select { |commit| commit[:author][:name] == user.name }.reverse_each do |commit|
-          user.commits.create(repo: event[:repo][:name],
-                              message: commit[:message],
-                              sha: commit[:sha],
-                              created_at: event[:created_at])
-        end
-      end
+  def self.event_data(nickname, name, github_id, github_secret)
+    connection = Faraday.new(url: "https://api.github.com")
+    user_events = parse(connection.get("users/#{nickname}/events/public?per_page=5&client_id=#{github_id}&client_secret=#{github_secret}"))
+    user_events.select! { |event| event[:type] == "PushEvent" }
+    user_events.flat_map do |event|
+      event[:payload][:commits].select { |commit| commit[:author][:name] == name }.reverse.map { |commit| [event, commit] }
     end
   end
 
   private
 
-  def parse(response)
+  def self.parse(response)
     JSON.parse(response.body, symbolize_names: true)
   end
 end
