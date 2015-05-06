df6e738498f0f3d1297d518c6acbe8b0708068eb
Austin Wood <i@austinwood.me>
2015-05-06 13:21:53 -0600

    Move stubbed HTTP requests from specs to private methods

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,4 +1,5 @@
 require "rails_helper"
+require "webmock/rspec"
 OmniAuth.config.test_mode = true
 
 RSpec.describe "User authentication", type: :feature do
@@ -14,13 +15,16 @@ RSpec.describe "User authentication", type: :feature do
     expect(page).to have_content("email@example.com")
   end
 
-  xit "can logout if logged in" do
+  it "can logout if logged in" do
     visit root_path
     mock_omniauth_user
 
     within(".nav") do
       find("#github-login").click
     end
+
+    mock_github_service_request
+
     within(".nav") do
       click_on "Logout"
     end
@@ -44,4 +48,24 @@ def mock_omniauth_user
       "token"  => "mock_token"
     }
   })
+end
+
+def mock_github_service_request
+  stub_request(:get, "https://api.github.com/users/mock_user/events/public?client_id=&client_secret=&per_page=5")
+  .with(headers: {"Accept"=>"*/*", "Accept-Encoding"=>"gzip;q=1.0,deflate;q=0.6,identity;q=0.3", "User-Agent"=>"Faraday v0.9.1"})
+  .to_return(status: 200, body: <<-JSON)
+    [ { "type":       "PushEvent",
+        "created_at": "2015-05-01T17:32:01Z",
+        "repo":       { "name": "vandelay-industries" },
+        "payload": {
+          "commits": [
+            { "sha": "c",
+              "author": { "name": "Art Vandelay" },
+              "message": "importing and exporting"
+            }
+          ]
+        }
+      }
+    ]
+  JSON
 end
