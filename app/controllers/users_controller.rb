804d2fbfee0cb6159eb848541265d3956fc92859
Austin Wood <i@austinwood.me>
2015-05-07 15:33:48 -0600

    Create edit and update actions used for adding a cohort to a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(nickname: params[:username])
   end
+
+  def edit
+    @user = User.find_by(nickname: params[:username])
+  end
+
+  def update
+    @user = User.find_by(nickname: params[:username])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:cohort_id)
+  end
 end
