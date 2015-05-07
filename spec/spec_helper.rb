41560ea24ede934e62bb0e2e2ac5fce20cee01f1
Austin Wood <i@austinwood.me>
2015-05-06 19:31:16 -0600

    Allow for real HTTP requests after all specs run so that I can submit my coverage to CodeClimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,19 @@ CodeClimate::TestReporter.start
 SimpleCov.start "rails"
 
 RSpec.configure do |config|
+  config.before(:all) do
+    WebMock.disable_net_connect!
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+  end
+
+  config.after(:all) do
+    WebMock.allow_net_connect!
   end
 end
