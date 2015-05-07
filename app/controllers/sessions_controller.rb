8c53c8a8b9caafa917182fd7b201a071b24a7ce7
Austin Wood <i@austinwood.me>
2015-05-07 15:32:53 -0600

    If the user is authenticating for the first time, send them to a form to add their cohort

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth(request.env["omniauth.auth"])
     if user
-      session[:user_id] = user.id
-      redirect_to user
+      if user.cohort == nil
+        redirect_to edit_user_path(user)
+      else
+        session[:user_id] = user.id
+        redirect_to user
+      end
     end
   end
 
